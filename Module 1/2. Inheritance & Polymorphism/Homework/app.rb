789c6bcb0f7af7c338431b450eb3a2bcf3a944f3
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-07-08 22:32:28 +0700

    feat: Finished Module 1 Session 2 homework

--- Module 1/2. Inheritance & Polymorphism/Homework/app.rb
+++ Module 1/2. Inheritance & Polymorphism/Homework/app.rb
@@ -3,10 +3,15 @@ require_relative 'mongol'
 require_relative 'person'
 require_relative 'villain'
 
-jin = Hero.new("Jin Sakai", 100, 50, 80)
+jin = Hero.new("Jin Sakai", 100, 50, true)
+yuna = Hero.new("Yuna", 90, 45, false)
+ishikawa = Hero.new("Sensei Ishikawa", 80, 60, false)
+
 mongol_archer = MongolArcher.new("Mongol Archer", 80, 40)
 mongol_spearman = MongolSpearman.new("Mongol Spearman", 120, 60)
 mongol_swordsman = MongolSwordsman.new("Mongol Swordsman", 100, 50)
+
+allies = [jin, yuna, ishikawa]
 villains = [mongol_archer, mongol_spearman, mongol_swordsman]
 
 i = 1
@@ -14,20 +19,38 @@ until (jin.die? || villains.empty?) do
     puts "========== Turn #{i} =========="
     puts "\n"
 
-    puts jin
+    allies.each do |ally|
+        puts ally
+    end
+    puts "\n"
+
     villains.each do |villain|
         puts villain
     end
     puts "\n"
 
-    jin.attack(villains[rand(villains.size)])
+    allies.each do |ally|
+        ally.pick_choices(villains, allies) if ally.can_control
+    end
+
     villains.each do |villain|
         villains.delete(villain) if villain.die? || villain.flee?
     end
+
+    allies.each do |ally|
+        ally.attack(villains[rand(villains.size)]) if !ally.can_control
+        villains.each do |villain|
+            villains.delete(villain) if villain.die? || villain.flee?
+        end
+    end
+
     puts "\n"
 
     villains.each do |villain|
-        villain.attack(jin)
+        villain.attack(allies[rand(allies.size)])
+        allies.each do |ally|
+            allies.delete(ally) if ally.die?
+        end
     end
     puts "\n"
 
--- Module 1/2. Inheritance & Polymorphism/Homework/hero.rb
+++ Module 1/2. Inheritance & Polymorphism/Homework/hero.rb
@@ -1,14 +1,18 @@
 require_relative 'person'
 
 class Hero < Person
-    def initialize(name, hitpoint, attack_damage, deflect_probability)
+    attr_reader :can_control
+
+    def initialize(name, hitpoint, attack_damage, can_control)
         super(name, hitpoint, attack_damage)
-        @deflect_probability = deflect_probability
+        @deflect_probability = 0.8
+        @can_control = can_control
+        @heal_point = 20
+        @initial_hitpoint = hitpoint
     end
 
     def deflect_attack?
-        current_percentage = rand(100) # generate a random number between 0 and 99
-        if current_percentage < @deflect_probability
+        if rand < @deflect_probability
             return true
         else
             return false
@@ -21,5 +25,64 @@ class Hero < Person
         else
             @hitpoint -= damage
         end
+    end
+
+    def pick_choices(villains, allies)
+        puts "As #{@name}, what do you want to do this turn?"
+        puts "1) Attack an enemy"
+        puts "2) Heal an ally"
+        choice = gets.to_i
+        puts "\n"
+
+        if choice == 1
+            pick_attack(villains)
+        elsif choice == 2
+            pick_heal(allies)
+        else
+            puts "Please pick a valid option"
+            pick_choices(villains, allies)
+        end
+    end
+
+    def pick_attack(villains)
+        puts "Which enemy you want to attack?"
+        villains.each_with_index do |villain, index|
+            puts "#{index + 1}) #{villain.name}"
+        end
+
+        choice = gets.to_i
+        puts "\n"
+        if choice <= villains.size
+            attack(villains[choice - 1])
+        else
+            puts "Please pick a valid option"
+            pick_attack(villains)
+        end
+    end
+
+    def pick_heal(allies)
+        puts "Which ally you want to heal?"
+        allies.each_with_index do |ally, index|
+            puts "#{index}) #{ally.name}" if ally != self
+        end
+
+        choice = gets.to_i
+        puts "\n"
+        if choice <= allies.size
+            heal_ally(allies[choice])
+        else
+            puts "Please pick a valid option"
+            pick_heal(allies)
+        end
+    end
+
+    def heal
+        @hitpoint += @heal_point
+        @hitpoint = @initial_hitpoint if @hitpoint > @initial_hitpoint
+    end
+
+    def heal_ally(other_person)
+        puts "#{@name} heals #{other_person.name}, restoring #{@heal_point} hitpoints"
+        other_person.heal
     end
 end
