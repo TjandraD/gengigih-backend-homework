0934daf11fa1b5cd74a527bd458f77d5e1d1bd48
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-07-22 21:09:49 +0700

    Refactor Module 3 Session 1 homework using MVC

--- Module 3/1. Database Connection/Homework/models/item.rb
+++ Module 3/1. Database Connection/Homework/models/item.rb
@@ -1,10 +1,82 @@
+require_relative '../db/db_connector'
+require_relative 'category'
+
 class Item
     attr_reader :id, :name, :price, :category
 
-    def initialize(id, name, price, category = nil)
+    def initialize(id = nil, name = nil, price = nil, category = nil)
         @id = id
         @name = name
         @price = price
         @category = category
+    end
+
+    def self.get_all_items
+        client = create_db_client
+        rawData = client.query("SELECT * FROM items")
+        items = Array.new
+        rawData.each do |data|
+            item = Item.new(data["id"], data["name"], data["price"])
+            items.push(item)
+        end
+        items
+    end
+
+    def insert_item()
+        client = create_db_client
+        client.query("INSERT INTO items(name, price) VALUES('#{@name}', #{@price})")
+    end
+
+    def get_item_with_category()
+        client = create_db_client
+        rawData = client.query(
+            "SELECT i.id, i.name, i.price, c.id AS \'category_id\', c.name AS \'category_name\'
+            FROM items i
+            RIGHT JOIN item_categories ic ON i.id = ic.item_id
+            JOIN categories c ON c.id = ic.category_id
+            WHERE i.id = #{@id}")
+    
+        data = rawData.first()
+        category = Category.new(data["category_id"], data["category_name"])
+        item = Item.new(data["id"], data["name"], data["price"], category)
+        item
+    end
+    
+    def self.get_items_with_categories()
+        client = create_db_client
+        rawData = client.query(
+            "SELECT i.id, i.name, i.price, c.id AS \'category_id\', c.name AS \'category_name\'
+            FROM items i
+            RIGHT JOIN item_categories ic ON i.id = ic.item_id
+            JOIN categories c ON c.id = ic.category_id
+            ORDER BY i.id ASC")
+        items = Array.new
+        rawData.each do |data|
+            category = Category.new(data["category_id"], data["category_name"])
+            item = Item.new(data["id"], data["name"], data["price"], category)
+            items.push(item)
+        end
+        items
+    end
+    
+    def insert_item_and_category()
+        client = create_db_client
+        client.query("INSERT INTO items(name, price) VALUES('#{@name}', #{@price})")
+    
+        item_id = client.last_id
+        client.query("INSERT INTO item_categories VALUES(#{item_id}, #{@category})")
+    end
+    
+    def update_item_and_category()
+        client = create_db_client
+        client.query("UPDATE items SET name = '#{@name}', price = #{@price} WHERE id = #{@id}")
+    
+        client.query("UPDATE item_categories SET category_id = #{@category} WHERE item_id = #{@id}")
+    end
+    
+    def delete_item()
+        client = create_db_client
+        client.query("DELETE FROM item_categories WHERE item_id = #{@id}")
+        client.query("DELETE FROM items WHERE id = #{@id}")
     end
 end
--- Module 3/1. Database Connection/Homework/web.rb
+++ Module 3/1. Database Connection/Homework/web.rb
@@ -1,15 +1,16 @@
 require 'sinatra'
-require_relative 'db_connector'
+require_relative 'models/item'
+require_relative 'models/category'
 
 get '/' do
-    items = get_items_with_categories
+    items = Item.get_items_with_categories
     erb :index, locals: {
         items: items
     }
 end
 
 get '/items/new' do
-    categories = get_all_categories
+    categories = Category.get_all_categories
     erb :create, locals: {
         categories: categories
     }
@@ -19,14 +20,16 @@ post '/items/new' do
     name = params["name"]
     price = params["price"]
     category = params["category"]
-    insert_item_and_category(name, price, category)
+    item = Item.new(nil, name, price, category)
+    item.insert_item_and_category
 
     redirect '/'
 end
 
 get '/items/details/:id' do
     item_id = params["id"]
-    item = get_item_with_category(item_id)
+    item = Item.new(item_id)
+    item = item.get_item_with_category
     erb :details, locals: {
         item: item
     }
@@ -34,27 +37,30 @@ end
 
 get '/items/edit/:id' do
     item_id = params["id"]
-    item = get_item_with_category(item_id)
-    categories = get_all_categories
+    item = Item.new(item_id)
+    item = item.get_item_with_category
+    categories = Category.get_all_categories
     erb :edit, locals: {
         item: item,
         categories: categories
     }
 end
 
-put '/items/edit/:id' do
+post '/items/edit/:id' do
     id = params["id"]
     name = params["name"]
     price = params["price"]
     category = params["category"]
-    update_item_and_category(id, name, price, category)
+    item = Item.new(id, name, price, category)
+    item.update_item_and_category
 
     redirect '/'
 end
 
 get '/items/delete/:id' do
     item_id = params["id"]
-    delete_item(item_id)
+    item = Item.new(item_id)
+    item.delete_item
 
     redirect '/'
 end
