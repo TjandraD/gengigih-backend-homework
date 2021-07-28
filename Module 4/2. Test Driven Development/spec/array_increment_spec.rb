f9b4a255084b0bdabdd0366eae2226217d3e1106
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-07-28 18:45:05 +0700

    [Module 4 Session 2] Finished [4, 0] test case

--- Module 4/2. Test Driven Development/spec/array_increment_spec.rb
+++ Module 4/2. Test Driven Development/spec/array_increment_spec.rb
@@ -8,8 +8,16 @@ RSpec.describe ArrayIncrement do
     it 'should returns [5, 6, 2] when given [5, 6, 1]' do
         array_increment = ArrayIncrement.new([5, 6, 1])
 
-        response = array_increment.plus_one
+        response = array_increment.return_array
 
         expect(response).to eq([5, 6, 2])
+    end
+
+    it 'should returns [4, 0] when given [3, 9]' do
+        array_increment = ArrayIncrement.new([3, 9])
+
+        response = array_increment.return_array
+
+        expect(response).to eq([4, 0])
     end
 end
---
+++ Module 4/2. Test Driven Development/src/array_increment.rb
@@ -0,0 +1,32 @@
+class ArrayIncrement
+    def initialize(params)
+        @first_num = params[0]
+        @second_num = params[1]
+        @third_num = params[2]
+    end
+
+    def return_array
+        if (@third_num.nil?)
+            two_inputs
+        else
+            three_inputs
+        end
+    end
+
+    def three_inputs
+        @third_num += 1
+        
+        response = [@first_num, @second_num, @third_num]
+    end
+
+    def two_inputs
+        @second_num += 1
+
+        if @second_num >= 10
+            @second_num -= 10
+            @first_num += 1
+        end
+        
+        response = [@first_num, @second_num]
+    end
+end
