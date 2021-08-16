e9e73c4722a2fa25e768295464bc8d4a1ceb123a
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-08-16 18:44:56 +0700

    [Module 6 Session 1] Hands-on: Refactor the bad code

--- Module 6/1. Clean Code/bad_code.rb
+++ Module 6/1. Clean Code/bad_code.rb
@@ -1,15 +1,13 @@
-# save user with auto generated password if user does not give password on registration.
- # default role is temp
- # return error if user email/username/phone is empty.
- def reg_usr(params)
-    a = Data.new(params)
-    a.role = "admin"
-    if a.password.blank?
-    a.gen_pass
-    end
-    raise Error::InvalidUserError if a.email.blank?||a.username.blank?||a.phone.blank?||!(a.email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i))||!(a.phone.match(/\A\+?[\d\s\-\.\(\)]+\z/))
-    raise Error::DuplicateUser if Data.query("select count(1) from users where phone=#{a.phone} or email=#{a.email}") > 0
-    a.save
-    a.sendmailcnfrm
-    a
+def register_user(params)
+    user = User.new(params)
+    user.role = "admin"
+
+    user.gen_pass if user.password.blank?
+
+    raise Error::InvalidUserError if user.invalid?
+    raise Error::DuplicateUser if user.duplicate?
+
+    user.save
+    user.send_email_confirmation
+    user
 end
